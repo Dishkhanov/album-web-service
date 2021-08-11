bf7b3d13d73f6476642ec1d7a6f406b58573d7b7
Daniel Ishkhanov <dishkhanov@sea.yearup.org>
2021-08-11 09:10:25 -0700

    Added JSON Changes

---
+++ data/albums-year.js
@@ -0,0 +1,55 @@
+{
+	"title"; "Albums by Year",
+	"albums";[
+			  {
+				"Year": 1996,
+				"Artist":"Tupac",
+				"Title":"All Eyez On Me",
+				"Sales": 5000000,
+				"Genre":"Rap",
+				"Image":"tupac.jpg"
+
+			  },
+
+			  {
+				"Year": 2006,
+				"Artist":"Amy Winehouse",
+				"Title":"Back to Black",
+				"Sales": 4000000,
+				"Genre":"Jazz",
+				"Image":"amy-winehouse.jpg"
+			  },
+
+			  {
+				"Year": 2010,
+				"Artist":"Bruno Mars",
+				"Title":"Doo-Wops & Hooligans",
+				"Sales": 1000000,
+				"Genre":"Pop",
+				"Image":"bruno-mars.jpg"
+			  },
+			  
+			  {
+				"Year": 2011,
+				"Artist":"Drake",
+				"Title":"Take Care",
+				"Sales": 7000000,
+				"Genre":"Hip Hop",
+				"Image":"drake.jpg"
+			  },
+			  
+			  {
+				"Year":2017,
+				"Artist":"G Yamazawa",
+				"Title":"Ego-Trip",
+				"Sales": 2000000,
+				"Genre":"Rap",
+				"Image":"g-yamazawa.jpg"
+			  },			  
+            ]
+        }
+
+
+
+
+
