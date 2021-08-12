f7bb84a1ab18b7db59abf35ba56ac8d80e014e61
Daniel Ishkhanov <dishkhanov@sea.yearup.org>
2021-08-11 23:35:31 -0700

    Fixes the JSON

--- index.php
+++ index.php
@@ -61,12 +61,13 @@ function bondJSON(data){
 
 	$('#films').html('');
 
-
+/*
     $.each(data.albums,function(i, item){
 		let str = bondTemplate(item);
 
 		$('<div></div>').html(str).appendTo('#films');
 	});
+    */
 //allows us to see all data on page
     let myData = JSON.stringify(data,null,4);
     myData = '<pre>' + myData + '</pre>';
